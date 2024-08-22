4a349f29abc8f7d1b34f958f49520e5de699ca79
erkabb <erkabb816@gmail.com>
2024-08-22 12:30:05 +0800

    client delete

--- src/components/maincontent.jsx
+++ src/components/maincontent.jsx
@@ -4,11 +4,37 @@ import Subheader from "./user-head";
 
 const Maincontent = () => {
   const [users, setUsers] = useState();
+
   const getEmployees = async () => {
     const res = await fetch("http://localhost:8000/users");
     const { users } = await res.json();
     setUsers(users);
   };
+
+  const createEmployee = async () => {
+    const res = await fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        firstname: "Бат",
+        lastname: "Наран",
+        email: "batbat@gmail.com",
+        position: "Developer",
+        profileImg: "https://img.daisyui.com/images/profile/demo/2@94.webp",
+      }),
+    });
+  };
+
+  const deleteEmp = async (userEid) => {
+    const res = await fetch(`http://localhost:8000/users/${userEid}`, {
+      method: "DELETE",
+    });
+  };
+
+  const editEmp = async (employee) => {
+    const res = await fetch(`http://localhost:8000/users/${employee}`);
+  };
+
   useEffect(() => {
     getEmployees();
   }, []);
@@ -20,11 +46,17 @@ const Maincontent = () => {
         </thead>
         <tbody>
           {users?.map((user) => {
-            return <UserRow user={user} />;
+            return (
+              <UserRow user={user} deleteEmp={deleteEmp} editEmp={editEmp} />
+            );
           })}
-          <UserRow />
         </tbody>
       </table>
+      <div>
+        <button className="btn btn-info btn-outline" onClick={createEmployee}>
+          Add employee
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,7 +1,4 @@
-import React from "react";
-
-const UserRow = ({ user }) => {
-  console.log("user:", user);
+const UserRow = ({ user, deleteEmp, editEmp }) => {
   return (
     <tr>
       <td>
@@ -19,12 +16,56 @@ const UserRow = ({ user }) => {
         </div>
       </td>
       <td>
-        <span className="badge badge-primary badge-outline badge-sm">g</span>
+        <span className="badge badge-primary badge-outline badge-sm">
+          {user.position}
+        </span>
       </td>
-      <td>g</td>
+      <td>{user.email}</td>
       <td className="flex gap-3 mt-4">
-        <button className="btn btn-outline btn-info btn-xs">Edit</button>
-        <button className="btn btn-outline btn-error btn-xs">delete</button>
+        {/* <button
+          className="btn btn-outline btn-info btn-xs "
+          onClick={() => editEmp(user.firstname)}
+        >
+          Edit
+        </button> */}
+        <button
+          className="btn btn-outline btn-xs"
+          onClick={() => document.getElementById("my_modal_5").showModal()}
+        >
+          Edit
+        </button>
+        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+          <div className="modal-box">
+            <label className="input input-bordered flex items-center gap-2">
+              Firstname
+              <input type="text" className="grow" placeholder="Daisy" />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              Email
+              <input
+                type="text"
+                className="grow"
+                placeholder="daisy@site.com"
+              />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              Position
+              <input type="text" className="grow" placeholder="Developer" />
+            </label>
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn">Close</button>
+                <button className="btn btn-success">Submit</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+        <button
+          className="btn btn-outline btn-error btn-xs"
+          onClick={() => deleteEmp(user.eid)}
+        >
+          delete
+        </button>
       </td>
     </tr>
   );
